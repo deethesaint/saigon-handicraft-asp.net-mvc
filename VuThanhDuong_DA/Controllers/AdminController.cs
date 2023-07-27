4c0a2b260724386ecbc8e2af3a11f11717dd9309
deethesaint <vtduong0912@gmail.com>
2023-07-27 12:53:47 +0700

    some frontend adjustments

--- VuThanhDuong_DA/Controllers/AdminController.cs
+++ VuThanhDuong_DA/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VuThanhDuong_DA.Models;
 
 namespace VuThanhDuong_DA.Controllers
 {
@@ -21,5 +22,75 @@ namespace VuThanhDuong_DA.Controllers
             return View();
         }
 
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(admin_account aa)
+        {
+            return View();
+        }
+
+        public ActionResult CategoryList()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryCreate()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryEdit()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryDelete()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+        public ActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        public ActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDelete()
+        {
+            return View();
+        }
+
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+        public ActionResult UserCreate()
+        {
+            return View();
+        }
+
+        public ActionResult UserEdit()
+        {
+            return View();
+        }
+
+        public ActionResult UserDelete()
+        {
+            return View();
+        }
     }
 }
