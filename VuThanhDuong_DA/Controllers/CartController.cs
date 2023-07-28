4931d64a7ece2211b327d12b91ebc071d39560f9
deethesaint <vtduong0912@gmail.com>
2023-07-28 22:58:24 +0700

    cart added

--- VuThanhDuong_DA/Controllers/CartController.cs
+++ VuThanhDuong_DA/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VuThanhDuong_DA.Models;
 
 namespace VuThanhDuong_DA.Controllers
 {
@@ -13,8 +14,30 @@ namespace VuThanhDuong_DA.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View(GetCart());
         }
 
+        public List<InCartProduct> GetCart()
+        {
+            if (Session["cart"] == null)
+                return new List<InCartProduct>();
+            return Session["cart"] as List<InCartProduct>;
+        }
+
+        public ActionResult AddProductIntoCart(int id)
+        {
+            List<InCartProduct> products = GetCart();
+            InCartProduct ICPrd = products.Find(prd => prd.Id == id);
+            if (ICPrd == null)
+            {
+                products.Add(new InCartProduct(id));
+            }
+            else
+            {
+                ICPrd.Amount++;
+            }
+            Session["cart"] = products;
+            return RedirectToAction("Details", "Product", new {ID = id});
+        }
     }
 }
---
+++ VuThanhDuong_DA/Models/InCartProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VuThanhDuong_DA.Models;
+
+namespace VuThanhDuong_DA.Models
+{
+    public class InCartProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int Amount { get; set; }
+        public InCartProduct(int id)
+        {
+            using (var dbContext = new SHSDBDataContext())
+            {
+                product prd = dbContext.products.SingleOrDefault(p => p.product_id == id);
+                Id = prd.product_id;
+                Name = prd.product_name;
+                Price = (decimal)prd.product_price;
+                Discount = (decimal)prd.product_discount;
+                Amount = 1;
+            }
+        }
+    }
+}
